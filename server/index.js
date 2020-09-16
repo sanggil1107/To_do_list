8009ccc1952263c3925110108ebd7c768a54be98
sanggil1107 <yangpang1107@gmail.com>
2020-09-17 00:00:03 +0900

    db connection

--- server/index.js
+++ server/index.js
@@ -2,12 +2,21 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 
-
+const db = mysql.createPool({
+  host: "127.0.0.1",
+  user: 'root',
+  password: '1qaz2wsx',
+  port: 3306,
+  database: 'management'
+})
 
 app.get('/', (req, res) => {
-  res.send('hello world');
-})
+  // const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception', 'good movie');"
+  // db.query(sqlInsert, (err, result) => {
+  //   res.send('hello world');
+  // })
+});
 
 app.listen(3001, () => {
   console.log('running on port 3001')
-})
+});
