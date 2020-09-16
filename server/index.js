435c4e272cfb6dbac2587c2fc64aba4d5e8f3809
sanggil1107 <yangpang1107@gmail.com>
2020-09-17 00:46:40 +0900

    get, post

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const mysql = require('mysql')
 
@@ -8,13 +10,27 @@ const db = mysql.createPool({
   password: '1qaz2wsx',
   port: 3306,
   database: 'management'
-})
+});
 
-app.get('/', (req, res) => {
-  // const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception', 'good movie');"
-  // db.query(sqlInsert, (err, result) => {
-  //   res.send('hello world');
-  // })
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/get', (req, res) => {
+  const sqlSelect = "SELECT * FROM movie_reviews";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  })
+});
+
+app.post("/api/insert", (req, res) => {
+  
+  const movieName = req.body.movieName
+  const movieReview = req.body.movieReview
+  const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)"
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+    console.log(result);
+  });
 });
 
 app.listen(3001, () => {
