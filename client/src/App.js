0f98553c9706c73ea439e059bf80c728552a8eef
sanggil1107 <yangpang1107@gmail.com>
2020-09-21 23:36:58 +0900

    todolist update delete

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 import axios from 'axios'
 import Template from './components/Template';
 import TodoList from './components/TodoList';
+import { MdAddCircle } from 'react-icons/md';
+import TodoInsert from './components/TodoInsert';
 
-
+let nextId = 4;
 const App = () => {
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [insertToggle, setInsertToggle] = useState(false);
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -23,9 +27,68 @@ const App = () => {
       checked: true
     }
   ]);
+
+  const onInsertToggle = () => {
+    if (selectedTodo) {
+      setSelectedTodo(null);
+    }
+    setInsertToggle(prev => !prev)
+  }
+
+  const onInsertTodo = (text) => {
+    if (text === "") {
+      return alert('할일을 입력하세요')
+    } else {
+      const todo = {
+        id: nextId,
+        text,
+        checked: false
+      };
+      setTodos(todos => todos.concat(todo));
+      nextId++;
+    }
+  };
+
+  const onCheckToggle = (id) => {
+    setTodos(todos => todos.map(todo => (
+      todo.id === id ? {...todo, checked: !todo.checked} : todo
+    )));
+  };
+
+  const onChangeSelectedTodo = (todo) => {
+    setSelectedTodo(todo);
+  }
+
+  const onRemove = (id) => {
+    onInsertToggle();
+    setTodos(todos => todos.filter(todo => todo.id !== id))
+  };
+
+  const onUpdate = (id, text) => {
+    onInsertToggle();
+    setTodos(todos => todos.map(todo => todo.id === id ? {...todo, text} : todo));
+  }
+
   return (
     <Template todoLength={todos.length}>
-      <TodoList todos={todos} />
+      <TodoList 
+        todos={todos} 
+        onCheckToggle={onCheckToggle} 
+        onInsertToggle={onInsertToggle}
+        onChangeSelectedTodo={onChangeSelectedTodo}
+      />
+      <div className="add-todo-button" onClick={onInsertToggle}>
+        <MdAddCircle />
+      </div>
+      {insertToggle && (
+        <TodoInsert 
+          selectedTodo={selectedTodo}
+          onInsertToggle={onInsertToggle}
+          onInsertTodo={onInsertTodo}        
+          onRemove={onRemove}
+          onUpdate={onUpdate}
+        />
+      )}
     </Template>
   );
 };
